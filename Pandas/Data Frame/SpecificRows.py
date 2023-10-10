6fa3aae78bcc6c3199cc7915324d074f9f94ced0
Shashi Vardhan Yasa <shashi.yasa7@gmail.com>
2023-10-10 15:33:06 +0530

    Create SpecificRows.py

---
+++ Pandas/Data Frame/SpecificRows.py
@@ -0,0 +1,16 @@
+# Write a Pandas program to select the specified columns and rows from a given data frame.
+# Select 'name' and 'score' columns in rows 1, 3, 5, 6 from the following data frame.
+
+import pandas as pd
+import numpy as np
+
+data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
+}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(data, index = labels)
+seleted_data = df.loc[['name', 'score'], ['b', 'd', 'e', 'g']]
+print(selected_data)
