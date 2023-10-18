583ec701fb9786fe2882a742b4a8c8d4e9d3f8b9
Shashi Vardhan Yasa <shashi.yasa7@gmail.com>
2023-10-18 12:43:39 +0530

    Delete Pandas directory

--- Pandas/Data Series/NumpyToSeries.py
+++
@@ -1,8 +0,0 @@
-# Write a Pandas program to convert a NumPy array to a Pandas series
-
-import pandas as pd
-import numpy as np
-
-data = np.array([10, 20, 30, 40, 50])
-df = pd.Series(data)
-print(df)
--- Pandas/Data Series/SeriesToList.py
+++
@@ -1,9 +0,0 @@
-# Write a Pandas program to convert a Panda module Series to Python list and it's type
-
-import pandas as pd
-
-data = pd.Series([1, 2, 3, 4, 5])
-my_list = data.tolist()
-list_type = type(my_list)
-print(my_list)
-print(list_type)
